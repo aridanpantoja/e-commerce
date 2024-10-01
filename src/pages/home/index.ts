56e54a071bafce02e45018b9d9fee8b29c0a7599
Aridan <aridanpantoja@gmail.com>
2024-10-01 18:30:16 -0300

    fix: re-render product list after remove action

--- src/pages/home/index.ts
+++ src/pages/home/index.ts
@@ -7,6 +7,10 @@ export default class Home {
         loadComponent(this.render())
 
         this.renderProducts()
+
+        window.addEventListener('changeProducts', () => {
+            this.renderProducts()
+        })
     }
 
     render(): string {
